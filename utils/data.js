fa5d69c781aec3026486a6b7f2600524e77a42b6
Gypsy Bird <110423762+hcruz77@users.noreply.github.com>
2022-10-18 21:25:03 -0400

    added seed data, error during run seed

--- utils/data.js
+++ utils/data.js
@@ -74,27 +74,28 @@ const names = [
   'Parker',
 ];
 
-const appDescriptions = [
-  'Decision Tracker',
-  'Find My Phone',
-  'Learn Piano',
-  'Starbase Defender',
-  'Tower Defense',
-  'Monopoly Money Manager',
-  'Movie trailers',
-  'Hello world',
-  'Stupid Social Media App',
-  'Notes',
-  'Messages',
-  'Email',
-  'Compass',
-  'Firefox',
-  'Running app',
-  'Cooking app',
-  'Poker',
-  'Deliveries',
+const thoughtList = [
+  'green is a great color',
+  'pants suck',
+  'dogs are better than cats',
+  'geeks are sexy',
+  'girls are better than boys',
+  'sleep is under rated',
+  'social media is a waste of time',
+  'pie is better than cake',
+  'unicorns should be real',
+  'is it grey or gray?',
+  'age is just a number',
+  'internet should be free',
+  'why are public restrooms so nasty?',
+  'the weather man is always wrong',
+  'how do they know what dinosaurs sounded like?',
+  'brownies',
+  'ghosts are real',
+  'puppy breath is the best',
 ];
 
+const users = [];
 // Get a random item given an array
 const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
@@ -103,16 +104,16 @@ const getRandomName = () =>
   `${getRandomArrItem(names)} ${getRandomArrItem(names)}`;
 
 // Function to generate random assignments that we can add to student object.
-const getRandomAssignments = (int) => {
+const getRandomThoughts = (int) => {
   const results = [];
   for (let i = 0; i < int; i++) {
     results.push({
-      assignmentName: getRandomArrItem(appDescriptions),
-      score: Math.floor(Math.random() * (99 - 70 + 1) + 70),
+      randomThought: getRandomArrItem(thoughtList),
+      username: getRandomName(),
     });
   }
   return results;
 };
 
 // Export the functions for use in seed.js
-module.exports = { getRandomName, getRandomAssignments };
+module.exports = { getRandomName, getRandomThoughts };
--- utils/seed.js
+++ utils/seed.js
@@ -1,51 +1,39 @@
 const connection = require('../config/connection');
-const { Course, Student } = require('../models');
-const { getRandomName, getRandomAssignments } = require('./data');
+const { Thought, User } = require('../models');
+const { getRandomName, getRandomThoughts } = require('./data');
 
 connection.on('error', (err) => err);
 
 connection.once('open', async () => {
   console.log('connected');
+  await Thought.deleteMany({});
+  await User.deleteMany({});
 
-  // Drop existing courses
-  await Course.deleteMany({});
-
-  // Drop existing students
-  await Student.deleteMany({});
-
-  // Create empty array to hold the students
-  const students = [];
-
-  // Loop 20 times -- add students to the students array
+  const users = [];
+  const thoughts = getRandomThoughts(20);
+ 
   for (let i = 0; i < 20; i++) {
-    // Get some random assignment objects using a helper function that we imported from ./data
-    const assignments = getRandomAssignments(20);
-
     const fullName = getRandomName();
     const first = fullName.split(' ')[0];
     const last = fullName.split(' ')[1];
-    const github = `${first}${Math.floor(Math.random() * (99 - 18 + 1) + 18)}`;
-
-    students.push({
+    
+    users.push({
       first,
       last,
-      github,
-      assignments,
+      thoughts,
     });
   }
 
-  // Add students to the collection and await the results
-  await Student.collection.insertMany(students);
-
-  // Add courses to the collection and await the results
-  await Course.collection.insertOne({
-    courseName: 'UCLA',
+  await User.collection.insertMany(users);
+  await Thought.collection.insertOne({
+    randomThought: 'pennies are stupid',
     inPerson: false,
-    students: [...students],
+    users: [...users],
   });
 
-  // Log out the seed data to indicate what should appear in the database
-  console.table(students);
+  
+  console.table(users);
+  console.table(thoughts);
   console.info('Seeding complete! 🌱');
   process.exit(0);
 });
